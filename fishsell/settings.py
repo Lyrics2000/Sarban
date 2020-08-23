d741e25fb24f6c3a5e73bbda62cdec44a80aa81d
Likimani <arjilikimani@gmail.com>
2020-08-24 00:18:45 +0300

    I

--- fishsell/settings.py
+++ fishsell/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static_project_file'),
-# ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media_cdn')
--- fishsell/wsgi.py
+++ fishsell/wsgi.py
@@ -1,10 +1,7 @@
-
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fishsell.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fishsell.settings")
 
-application = get_wsgi_application()
-
+application = DjangoWhiteNoise(get_wsgi_application())
