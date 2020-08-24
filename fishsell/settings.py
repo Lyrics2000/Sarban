8afc27d9e59c3215fe207f8eb8a84528135b868e
Likimani <arjilikimani@gmail.com>
2020-08-24 07:07:43 +0300

    medias

--- fishsell/settings.py
+++ fishsell/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'fishsell.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'Host' : 'ec2-107-20-104-234.compute-1.amazonaws.com',
+        'NAME' :'db0e40oghro5ea',
+        'USER' : 'bnezixmerlgdsv',
+        'PORT'  : 5432,
+        'PASSWORD' : 'a629fa0212d23bf4cc9f63e7ae30e1c36596d1d85dcbd2441c1796f5bb670969',
     }
 }
 
@@ -134,6 +138,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'fishsell.storage_backends.MediaStorage' 
 
 
 # AWS_ACCESS_KEY_ID = 'AKIA3T7FB5INOG27IH5T'
