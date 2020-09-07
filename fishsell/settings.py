c58b47f9ba3a36036076a34f436de1cbb7455449
Likimani <arjilikimani@gmail.com>
2020-09-07 22:38:12 +0300

    oks

--- fishsell/settings.py
+++ fishsell/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '1=y5q)niwp$ve%1ywl)j&l!$rwtoo-twd&aunswp1z!v^&t3xq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sleepy-savannah-28536.herokuapp.com','127.0.0.1']
 
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'fishsell.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'HOST' : 'ec2-107-20-104-234.compute-1.amazonaws.com',
-#         'NAME' :'db0e40oghro5ea',
-#         'USER' : 'bnezixmerlgdsv',
-#         'PORT'  : 5432,
-#         'PASSWORD' : 'a629fa0212d23bf4cc9f63e7ae30e1c36596d1d85dcbd2441c1796f5bb670969',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST' : 'ec2-107-20-104-234.compute-1.amazonaws.com',
+        'NAME' :'db0e40oghro5ea',
+        'USER' : 'bnezixmerlgdsv',
+        'PORT'  : 5432,
+        'PASSWORD' : 'a629fa0212d23bf4cc9f63e7ae30e1c36596d1d85dcbd2441c1796f5bb670969',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -137,14 +137,14 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static_project_file'),
-]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static_cdn')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static_project_file'),
+# ]
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static_cdn')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media_cdn')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media_cdn')
 
 EMIAL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'ambetsaachongo@gmail.com'
@@ -154,22 +154,22 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = 'Lyrics254'
 
 
-# #Production
-# AWS_ACCESS_KEY_ID = 'AKIA3T7FB5INOG27IH5T'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'handlings254'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_LOCATION = 'static'
+#Production
+AWS_ACCESS_KEY_ID = 'AKIA3T7FB5INOG27IH5T'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'handlings254'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static_project_file'),
-# ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# DEFAULT_FILE_STORAGE = 'fishsell.storage_backends.MediaStorage' 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_project_file'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'fishsell.storage_backends.MediaStorage' 
 
 
 
